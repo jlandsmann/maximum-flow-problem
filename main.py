b13aba7aa28a3956ecc9ee258d6cef271011639d
jlandsmann <git.landsmann@gmail.com>
2020-11-25 08:10:57 +0100

    optimization

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from utils.graph_generator import GraphGenerator
-from utils.graph_printer import GraphPrinter
-from utils.max_flow_problem_solver import MaxFlowProblemSolver
+import time
+
 from utils.edmonds_karp import EdmondsKarp
+from utils.graph_generator import GraphGenerator
+from utils.max_flow_problem_solver import MaxFlowProblemSolver
 
-G = GraphGenerator.generate(max_nodes=6, max_edges=30, max_weight=20)
-GraphPrinter.print_graph(G)
-solver = MaxFlowProblemSolver(G.copy())
-print(solver.solve(1, 6))
+max_node_count = 1000
+max_edge_count = 6000
 
-solver = EdmondsKarp(G.copy(), 1, 6)
+print("generation")
+start = time.time()
+G = GraphGenerator.generate(max_nodes=max_node_count, max_edges=max_edge_count, max_weight=60)
+# GraphPrinter.print_graph(G)
+end = time.time()
+print(end - start)
+
+# print("simple implementation")
+# start = time.time()
+# solver = MaxFlowProblemSolver(G.copy())
+# print(solver.solve(1, max_node_count))
+# end = time.time()
+# print(end - start)
+
+print("edmonds karp")
+start = time.time()
+solver = EdmondsKarp(G.copy(), 1, max_node_count)
 print(solver.solve())
+end = time.time()
+print(end - start)
--- utils/graph_generator.py
+++ utils/graph_generator.py
@@ -8,12 +8,11 @@ class GraphGenerator:
     @staticmethod
     def generate(max_nodes=6, max_edges=12, max_weight=5) -> nx.DiGraph:
         graph = nx.DiGraph()
-        for i in range(1, max_nodes):
-            graph.add_node(i)
+        graph.add_nodes_from(range(1, max_nodes))
         for i in range(1, max_edges):
             start = random.randint(1, max_nodes)
             end = random.randint(1, max_nodes)
             capacity = weight = random.randint(1, max_weight)
             flow = 0
-            graph.add_weighted_edges_from(ebunch_to_add=[(np.minimum(start, end), np.maximum(start, end), weight)], capacity=capacity, flow=flow)
+            graph.add_edge(np.minimum(start, end), np.maximum(start, end), weight=weight, capacity=capacity, flow=flow)
         return graph
